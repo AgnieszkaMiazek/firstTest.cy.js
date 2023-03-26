3e43f4343ca15234d65680f42b95ebb2a1c8d026
AgnieszkaMiazek <123297831+AgnieszkaMiazek@users.noreply.github.com>
2023-03-26 18:56:39 +0200

    firstTest

---
+++ 1-getting-started/firstTest.cy.js
@@ -0,0 +1,24 @@
+/// <reference types="cypress" />
+
+describe("Pierwszy test", () => {
+  it("login", () => {
+    cy.visit("https://www.edu.goit.global/account/login");
+    cy.get("#user_email")
+      .type("user888@gmail.com")
+      .should("have.value", "user888@gmail.com");
+    cy.get("#user_password")
+      .type("1234567890")
+      .should("have.value", "1234567890");
+    cy.get("1234567890").click();
+  });
+  it("logowanie na stronę goIT", () => {
+    cy.visit("https://www.edu.goit.global/account/login");
+    cy.get("#user_email")
+      .type("testowyqa@qa.team")
+      .should("have.value", "testowyqa@qa.team");
+    cy.get("#user_password")
+      .type("QA!automation-1")
+      .should("have.value", "QA!automation-1");
+    cy.get("QA!automation-1").click();
+  });
+});
